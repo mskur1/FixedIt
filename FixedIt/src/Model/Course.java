7df150731e356893ed5f163006883e1b2eea4b20
Mat Jones <mjones44@ycp.edu>
2016-02-25 21:02:08 -0500

    Updated Course class

--- FixedIt/src/Model/Course.java
+++ FixedIt/src/Model/Course.java
@@ -1,10 +1,16 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Course {
-	private int CRN, credits, capacity, seatsRemain, seatsFilled;
-	private String courseAndSection, title, type, location, days, time;
+	private int CRN, capacity, seatsRemain, seatsFilled;
+	double credits;
+	private String courseAndSection, title, type, days, time, beginEnd;
+	private ArrayList<String> instructors, locations;
 	
-	public Course(int CRN, int credits, int capacity, int seatsRemain, int seatsFilled, String courseAndSection, String title, String type, String location, String days, String time){
+	public Course(int CRN, String courseAndSection, String title, double credits, String type, String days, 
+			String time, String location, String instructor, int capacity, int seatsRemain, 
+			int seatsFilled, String beginEnd){
 		this.CRN=CRN;
 		this.credits=credits;
 		this.capacity=capacity;
@@ -13,12 +19,44 @@ public class Course {
 		this.courseAndSection=courseAndSection;
 		this.title=title;
 		this.type=type;
-		this.location=location;
 		this.days=days;
 		this.time=time;
+		instructors=new ArrayList<String>();
+		instructors.add(instructor);
+		locations=new ArrayList<String>();
+		this.locations.add(location);
+		this.beginEnd=beginEnd;
+	}
+	@Override
+	public String toString() {
+		return "Course [CRN=" + CRN + ", courseAndSection=" + courseAndSection + ", title=" + title + ", credits="
+				+ credits + ", type=" + type + ", days=" + days + ", time=" + time + ", locations=" + locations
+				+ ", instructors=" + instructors + ", capacity=" + capacity + ", seatsRemain=" + seatsRemain
+				+ ", seatsFilled=" + seatsFilled + ", beginEnd=" + beginEnd + "]";
 	}
 	public Course(){
-		System.out.print("Course object created.");
+		locations=new ArrayList<String>();
+		instructors=new ArrayList<String>();
+	}
+	
+	
+	public void addInstructor(String instructor){
+		instructors.add(instructor);
+	}
+	public void addLocation(String location){
+		locations.add(location);
+	}
+	public String getBeginEnd() {
+		return beginEnd;
+	}
+	public void setBeginEnd(String beginEnd) {
+		this.beginEnd = beginEnd;
+	}
+	public ArrayList<String> getInstructors() {
+		return instructors;
+	}
+	public void setInstructors(ArrayList<String> instructors) {
+		this.instructors = instructors;
 	}
 	public int getCRN() {
 		return CRN;
@@ -26,11 +64,11 @@ public class Course {
 	public void setCRN(int CRN) {
 		this.CRN = CRN;
 	}
-	public int getCredits() {
+	public double getCredits() {
 		return credits;
 	}
-	public void setCredits(int credits) {
-		this.credits = credits;
+	public void setCredits(double d) {
+		this.credits = d;
 	}
 	public int getCapacity() {
 		return capacity;
@@ -68,11 +106,11 @@ public class Course {
 	public void setType(String type) {
 		this.type = type;
 	}
-	public String getLocation() {
-		return location;
+	public ArrayList<String> getLocation() {
+		return locations;
 	}
-	public void setLocation(String location) {
-		this.location = location;
+	public void setLocation(ArrayList<String> locations) {
+		this.locations = locations;
 	}
 	public String getDays() {
 		return days;
@@ -85,5 +123,5 @@ public class Course {
 	}
 	public void setTime(String time) {
 		this.time = time;
-	}	
+	}
 }
